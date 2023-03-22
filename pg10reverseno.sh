aec8c832ea8304fe364ebd3335593443216e0324
adishasivadas <adishasivadas9633>
2023-03-22 14:46:47 +0530

    to find reverse of a number

--- pg10reverseno.sh
+++ pg10reverseno.sh
@@ -1,14 +1,14 @@
 echo "enter a number"
-read n
+read a
 rev=0
-while [ $n -gt 0 ]
+while [ $a -gt 0 ]
 do
-d=`expr $n % 10`
-if [$d -eq 0 -n $rev -eq 0]
+d=`expr $a % 10`
+if [ $d -eq 0 -a $rev -eq 0 ]
 then
 printf "$d"
 fi
 rev=`expr $rev \* 10 + $d`
-n=`expr $n / 10`
+a=`expr $a / 10`
 done
 echo "REVERSE IS : $rev"
